284fe4545c4383a01fb44bb015e6a3ab51dc92a0
alextopand <erdekov.sascha@yandex.ru>
2025-04-05 08:55:22 +0300

    Start settings

---
+++ backend/config/settings/dev.py
@@ -0,0 +1,16 @@
+from .base import *
+
+DEBUG = True
+SECRET_KEY = 'dev-secret-key'
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'db'),
+        'USER': os.getenv('POSTGRES_USER', 'root'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'root'),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
+    }
+}
---
+++ backend/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+echo "Running server..."
+exec "$@"
