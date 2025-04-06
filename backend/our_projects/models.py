06b26e729e4f58079e4dd9dc6729d4ce012b5c4b
alextopand <erdekov.sascha@yandex.ru>
2025-04-06 19:47:36 +0300

    our projects gallery for main page

--- backend/our_projects/models.py
+++ backend/our_projects/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 class OurProject(models.Model):
     name = models.CharField(max_length=100, verbose_name="Название")
+    announcement = models.TextField(verbose_name="Анонс")
     description = models.TextField(verbose_name="Описание")
     image = models.ImageField(upload_to="our_projects/", verbose_name="Фотография")
     order = models.PositiveIntegerField(default=1, verbose_name="Сортировка")
 
     class Meta:
-        db_table = "Наши проекты"
+        db_table = "Our_Projects"
         verbose_name = "Проект"
         verbose_name_plural = "Проекты"
